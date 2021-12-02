89846f9198dc68ed096e5f5494a3771803379ced
Matt C <me@mitt.dev>
2021-12-02 11:55:35 +0000

    Complete Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
+using MoreLinq;
+using static System.IO.File;
+
+var input = ReadLines(@"./input.txt").Select(int.Parse).ToArray();
+var lastReading = input[0];
+var ctr = 0;
+
+foreach (var reading in input.Skip(1))
+{
+    if (reading > lastReading) ctr++;
+    lastReading = reading;
+}  
+
+Console.WriteLine($"Single Increases: {ctr}");
+
+var windows = input.Window(3).Select(x => x.Sum()).ToArray();
+var lastWindow = windows[0];
+ctr = 0;
+
+foreach (var window in windows.Skip(1))
+{
+    if (window > lastWindow) ctr++;
+    lastWindow = window;
+}
+
+Console.WriteLine($"Window Increases: {ctr}");
