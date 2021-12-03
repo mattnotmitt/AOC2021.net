5aa2c2ec591c0d7c2c1106c75a6344f17bc48bac
Matt Coomber <matt.coomber@dysismedical.com>
2021-12-03 11:29:10 +0000

    Day 3! (was hard)

---
+++ Day3Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using Day3;
+using NUnit.Framework;
+
+namespace Day3Test
+{
+    public class Tests
+    {
+        static string testData = "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010";
+        [SetUp]
+        public void Setup()
+        {
+        
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var input = testData.Split("\n").Select(x => x.ToCharArray().Select(y => y == '1').ToArray()).ToArray();
+            Assert.AreEqual(23, LifeSupport.GetRating(input, true));
+            Assert.AreEqual(10, LifeSupport.GetRating(input, false));
+            Assert.Pass();
+        }
+    }
+}
